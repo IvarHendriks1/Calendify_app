5d92fda8f95f606f0594e412ac72fef90a34fc3e
Joost <116558339+jostitost1@users.noreply.github.com>
2024-10-01 12:12:47 +0200

    Update Mycontext.cs

--- Project/Mycontext.cs
+++ Project/Mycontext.cs
@@ -1,3 +1,4 @@
+namespace NewContext;
 class MyContext
 {
 
@@ -9,10 +10,34 @@ class MyContext
     }
     private void DefineEndPoints()
     {
-        app.MapPost("/Login", (string user, string pass) => UserLogin);
+        app.MapPost("/Login/{user}/{pass}", UserLogin);
+        app.MapGet("/Login/{user}", IsUserLogged);
+    }
+
+    public IResult UserLogin(string user, string pass) {
+        if (user is null || pass is null){
+            //if (usernotfound) Results.NotFound($"No user found with username: {user}")
+            string user_pass = "hi"; //get password of the user from database
+            if (pass == user_pass){
+                Results.Ok("Login Succes");
+                //set user to logged in
+            }
+            return Results.Ok("Login failed incorrect password");
+
+        }
+        return Results.NoContent();
+    }
+
+    public IResult IsUserLogged(string user){
+        if (user is null){
+            bool isuserlogged = false; //check in the database if user is logged
+            
+            return Results.Ok(isuserlogged);
+            
+        }
+        return Results.NoContent();
     }
 
 }
 
 
-// def UserLogin
