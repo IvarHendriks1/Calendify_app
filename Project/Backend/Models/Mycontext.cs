c6a2afd4091b1e6e4a096b85dd76bc01269c2d08
IvarHendriks1 <1057848@hr.nl>
2024-11-05 10:08:56 +0100

    Revert "Merge pull request #4 from IvarHendriks1/Alper"
    This reverts commit eb1bd7d49085cbd937fcfddbdaf052c1ea6b3adf, reversing
    changes made to 8cb27affe24f7b7b66cc39f00f088a5b12e5c827.

--- Project/Backend/Models/Mycontext.cs
+++ Project/Backend/Models/Mycontext.cs
@@ -7,31 +7,28 @@ namespace CalendifyApp.Models
     {
         private readonly IConfiguration configuration;
 
+
         public MyContext(IConfiguration configuration)
         {
             this.configuration = configuration;
         }
 
-        // Configure the database to use SQLite
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlite(configuration.GetConnectionString("DefaultConnection"));
         }
 
-        // DbSets for each table in your database
         public DbSet<Event> Events { get; set; }
         public DbSet<Attendance> Attendance { get; set; }
         public DbSet<Admin> Admin { get; set; }
         public DbSet<Event_Attendance> event_Attendance { get; set; }
         public DbSet<User> Users { get; set; }
 
-        // OnModelCreating method to configure the model and seed data
+
+
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
-
-            // Call the seed data method to populate the database with dummy data
-            modelBuilder.SeedData();
         }
     }
 }
