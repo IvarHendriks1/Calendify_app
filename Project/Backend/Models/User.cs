585f0e6a92945f57e5d0b3aafc727a0b0c8568bc
Asmz <aziaatyt@gmail.com>
2025-01-08 10:37:35 +0100

    Changed Models, Removed Database

--- Project/Backend/Models/User.cs
+++ Project/Backend/Models/User.cs
@@ -1,20 +1,15 @@
-﻿using System.Collections.Generic;
-
-namespace CalendifyApp.Models
+﻿namespace CalendifyApp.Models
 {
     public class User
     {
         public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string RecurringDays { get; set; } // Assuming this is a string to store days in a specific format.
 
-        public required string First_name { get; set; }
-
-        public required string Last_name { get; set; }
-
-        public required string Email { get; set; }
-
-        public required string Password { get; set; }
-
-        // A comma-separated string that could look like this: "mo,tu,we,th,fr"
-        public required string Recurring_days { get; set; }
+        public ICollection<Attendance> Attendances { get; set; }
+        public ICollection<EventAttendance> EventAttendances { get; set; }
     }
 }
