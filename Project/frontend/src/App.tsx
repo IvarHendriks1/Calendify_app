cb6ebfdff1cb45783f79af16e2bf67fdfe70d725
manojlo14 <markovicmane04@gmail.com>
2025-01-09 19:23:44 +0100

    heb nogsteeds error genaamd Cannot read properties of null (reading 'useRef')

--- Project/frontend/src/App.tsx
+++ Project/frontend/src/App.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router, Routes, Route, useNavigate } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { UserInput } from './LoginPage';
+import { EventCreator } from './EvenCreator';
 import { GreetingPage } from './CalenderPage';
 
 const App: React.FC = () => {
@@ -10,16 +11,22 @@ const App: React.FC = () => {
       <Routes>
         <Route path="/" element={<UserInput />} />
         <Route path="/greeting" element={<GreetingPage />} />
+        <Route path="/event" element={<EventCreator />} />
       </Routes>
     </Router>
   );
 };
 
+// const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <App />
 );
+
 
 export default App;
