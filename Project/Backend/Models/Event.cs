d34fe81095c8f4784c888cee8540254909daf48d
alpersonmez <aziaatyt@gmail.com>
2025-01-13 21:20:41 +0100

    Move EventDTO's to Event.cs

--- Project/Backend/Models/Event.cs
+++ Project/Backend/Models/Event.cs
@@ -1,4 +1,5 @@
 using System.Text.Json.Serialization;
+using System.ComponentModel.DataAnnotations;
 
 namespace CalendifyApp.Models
 {
@@ -16,4 +17,39 @@ namespace CalendifyApp.Models
         [JsonIgnore] // Prevent cyclic reference
         public ICollection<EventAttendance> EventAttendances { get; set; }
     }
+
+    public class DetailedEventDTO
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime Date { get; set; }
+        public TimeSpan StartTime { get; set; }
+        public TimeSpan EndTime { get; set; }
+        public string Location { get; set; }
+        public bool AdminApproval { get; set; }
+
+        public List<EventAttendanceDTO> EventAttendances { get; set; }
+    }
+
+    public class DTOEvent
+    {
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string Description { get; set; }
+        [Required]
+        public DateTime Date { get; set; }
+        [Required]
+        public TimeSpan StartTime { get; set; }
+        [Required]
+        public TimeSpan EndTime { get; set; }
+        [Required]
+        public string Location { get; set; }
+        [Required]
+        public bool AdminApproval { get; set; }
+    }
+
 }
+
+    
--- Project/Backend/Models/EventAttendanceDTO.cs
+++
@@ -1,7 +0,0 @@
-public class EventAttendanceDTO
-{
-    public int UserId { get; set; }
-    public string UserName { get; set; }
-    public int Rating { get; set; }
-    public string Feedback { get; set; }
-}
